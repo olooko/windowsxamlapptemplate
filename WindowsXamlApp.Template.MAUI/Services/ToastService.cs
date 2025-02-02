4367b3ff57c75ab69c27c9f549267be032b5fba9
codeblender <codeblender@gmail.com>
2025-02-02 22:37:11 +0900

    maui toast 2

--- WindowsXamlApp.Template.MAUI/Services/ToastService.cs
+++ WindowsXamlApp.Template.MAUI/Services/ToastService.cs
@@ -2,36 +2,61 @@
 
 namespace WindowsXamlApp.Template.MAUI.Services
 {
+    public class ToastWindowOverlay : WindowOverlay
+    {
+        private IWindowOverlayElement _toastWindowOverlayElement;
+
+        public ToastWindowOverlay(IWindow window) : base(window)
+        {
+            _toastWindowOverlayElement = new ToastWindowOverlayElement(this);
+            AddWindowElement(_toastWindowOverlayElement);
+        }
+
+        class ToastWindowOverlayElement : IWindowOverlayElement
+        {
+            readonly IWindowOverlay _overlay;
+
+            RectF _backgroundRect = new RectF();
+            Color _backgroundColor = Color.FromArgb("FF000000");
+            Color _loadingSpinnerColor = Colors.White;
+
+            public ToastWindowOverlayElement(IWindowOverlay overlay, Color? background = null, Color? spinner = null)
+            {
+                _overlay = overlay;
+                if (background != null)
+                    this._backgroundColor = background;
+                if (spinner != null)
+                    _loadingSpinnerColor = spinner;
+            }
+
+            public bool Contains(Point point)
+                => this._backgroundRect.Contains(new Point(point.X / _overlay.Density, point.Y / _overlay.Density));
+
+            public void Draw(ICanvas canvas, RectF dirtyRect)
+            {
+                this._backgroundRect = new RectF(0, 0, 200, 200); //dirtyRect;
+                canvas.FillColor = _backgroundColor;
+                canvas.StrokeColor = _loadingSpinnerColor;
+                canvas.FontColor = _loadingSpinnerColor;
+                canvas.FontSize = 45f;
+
+                canvas.FillRectangle(this._backgroundRect);
+
+                
+                canvas.DrawString("Now Loading...", dirtyRect.Width / 2, dirtyRect.Height / 2, HorizontalAlignment.Center);
+            }
+        }
+
+    }
+
+
+
     public sealed class ToastService : IToastService
     {
         public void Show(string message)
         {
             var window = App.Current!.Windows[0];
-
-            window.AddOverlay(new LoadingOverlay(window));
-
-            var currentPage = window.Page as ContentPage;
-
-            if (currentPage != null)
-            {
-                //currentPage.AddLogicalChild(new Border
-                //{
-                //    WidthRequest = 300,
-                //    HeightRequest = 200,
-                //    BackgroundColor = Colors.AliceBlue,
-                //    Content = new Label { Text = message },
-                //});
-                //var newPage = new ContentPage
-                //{
-                //    BackgroundColor = Color.FromArgb("#88000000"),
-                //    Content = userDialog,
-                //};
-
-                //await currentPage.Navigation.PushModalAsync(newPage, false);
-                //result = await userDialog.WaitAsync();
-            }
-
-            //System.Diagnostics.Trace.WriteLine(App.Current.MainPage.
+            window.AddOverlay(new ToastWindowOverlay(window));
         }
     }
 }
