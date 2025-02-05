95b94c3f388101cb5177abd3c11b675d2e1e817a
김호섭 <hskim@beaverworksinc.com>
2025-02-05 18:08:34 +0900

    toast show,hide (winui,wpf)

--- WindowsXamlApp.Template.WPF/Services/ToastService.cs
+++ WindowsXamlApp.Template.WPF/Services/ToastService.cs
@@ -1,4 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 using WindowsXamlApp.Common.Services;
 
 namespace WindowsXamlApp.Template.WPF.Services
@@ -8,7 +11,34 @@ namespace WindowsXamlApp.Template.WPF.Services
         public void Show(string message)
         {
             var window = Ioc.Default.GetRequiredService<MainWindow>();
+            (window.ToastContent.Child as TextBlock)!.Text = message;
+
             window.ToastContent.Visibility = System.Windows.Visibility.Visible;
+
+            var doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = 1;
+            doubleAnimation.Duration = TimeSpan.FromSeconds(3);
+            doubleAnimation.AutoReverse = true;
+
+            var powerEase = new PowerEase();
+            powerEase.Power = 10;
+            powerEase.EasingMode = EasingMode.EaseOut;
+            doubleAnimation.EasingFunction = powerEase;
+
+            Storyboard.SetTargetName(doubleAnimation, window.ToastContent.Name);
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath(Border.OpacityProperty));
+
+            var showToastStoryBoard = new Storyboard();
+            showToastStoryBoard.Children.Add(doubleAnimation);
+            showToastStoryBoard.Completed += ShowToastStoryBoard_Completed;
+            showToastStoryBoard.Begin(window.ToastContent);
+        }
+
+        private void ShowToastStoryBoard_Completed(object? sender, EventArgs e)
+        {
+            var window = Ioc.Default.GetRequiredService<MainWindow>();
+            window.ToastContent.Visibility = System.Windows.Visibility.Collapsed;
         }
     }
 }
--- WindowsXamlApp.Template.WinUI/Services/ToastService.cs
+++ WindowsXamlApp.Template.WinUI/Services/ToastService.cs
@@ -1,4 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml;
+using System;
 using WindowsXamlApp.Common.Services;
 
 namespace WindowsXamlApp.Template.WinUI.Services
@@ -8,7 +12,36 @@ namespace WindowsXamlApp.Template.WinUI.Services
         public void Show(string message)
         {
             var window = Ioc.Default.GetRequiredService<MainWindow>();
+            (window.ToastContent.Child as TextBlock)!.Text = message;
+
             window.ToastContent.Visibility = Microsoft.UI.Xaml.Visibility.Visible;
+
+            var doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = 1;
+            doubleAnimation.Duration = TimeSpan.FromSeconds(3);
+            doubleAnimation.AutoReverse = true;
+
+            var powerEase = new PowerEase();
+            powerEase.Power = 10;
+            powerEase.EasingMode = EasingMode.EaseOut;
+            doubleAnimation.EasingFunction = powerEase;
+
+            Storyboard.SetTargetName(doubleAnimation, window.ToastContent.Name);
+            Storyboard.SetTargetProperty(doubleAnimation, "Border.Opacity");
+            Storyboard.SetTarget(doubleAnimation, window.ToastContent);
+
+            var showToastStoryBoard = new Storyboard();
+            showToastStoryBoard.Children.Add(doubleAnimation);
+            showToastStoryBoard.Completed += ShowToastStoryBoard_Completed;
+            
+            showToastStoryBoard.Begin();
+        }
+
+        private void ShowToastStoryBoard_Completed(object? sender, object e)
+        {
+            var window = Ioc.Default.GetRequiredService<MainWindow>();
+            window.ToastContent.Visibility = Microsoft.UI.Xaml.Visibility.Collapsed;
         }
     }
 }
